393d15019d069f2410cb0cd11a79e3da091167cf
duruchima <duruchima1@gmail.com>
2020-04-29 11:11:17 -0400

    one hour in

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@
 // submit patch request to update dog information **remember to prevent default on submit
 // perform new get fetch on dog api to obtain most up to date information
 const dogApi = 'http://localhost:3000/dogs'
-const editBttn = document.getElementsByTagName('button')
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    
     renderDogs()
     console.log('test')
     function renderDogs(){
@@ -21,9 +22,24 @@ document.addEventListener('DOMContentLoaded', () => {
     let table = document.getElementById('table-body')
     let dogRow = table.insertRow(0)
     dogRow.dataset.id = `${dog.id}`
-    // let cell1 = dogRow.insertCell(0)
-    // let cell2 = dogRow.insertCell(1)
     dogRow.innerHTML = `<tr><td>${dog.name}</td> <td>${dog.breed}</td> 
     <td>${dog.sex}</td> 
-    <td><button>Edit</button></td></tr>`}
+    <td><button class='edit'>Edit</button></td></tr>`}
+    
+    const editBttn = Array.from(document.getElementsByClassName('edit'))
+    editBttn.forEach((event) => {
+        makeListener(event)
+    })
+    function makeListener (event){event.addEventListener('click', () => {
+        let editDog = false
+        console.log('listening')
+        const dogForm = document.getElementById('dog-form')
+        editDog = !editDog
+        if (editDog) {
+            dogForm.style.display = "block"
+        } else {
+            dogForm.style.display = "none"
+        }
+
+    })}
 })
